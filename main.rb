9add576c785571a40d40da2594ce166cd654d374
Tina Ellis <tina.ellis@getg5.com>
2015-04-14 07:59:01 -0700

    set up the initial gameplay

--- main.rb
+++ main.rb
@@ -7,15 +7,20 @@ use Rack::Session::Cookie, :key => 'rack.session',
                             :path => '/',
                             :secret => 'brains'
 
-
 get '/' do
-  "Good morning World!"
+  redirect "/form"
 end
 
-get '/spiderman' do
-  erb :spiderman
+get '/form' do
+  erb :form
 end
 
-post '/forms/login' do
-  erb :forms
+post '/bet' do
+  params['username']
 end
+
+get '/bet' do
+  erb :bet
+end
+
+
